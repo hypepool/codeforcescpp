9d91ff9b54579028ce0fad1d19b91e6f3f7264cb
hypepool <saminurpurob@gmail.com>
2025-04-25 11:45:02 +0600

    Add files via upload

---
+++ st.chroma.cpp
@@ -0,0 +1,48 @@
+/*
+ 
+      ██   ██ ██    ██ ██████  ███████
+      ██   ██ ██    ██ ██  ██  ██
+      ███████ ████████ ██████  █████
+      ██   ██    ██    ██      ██
+      ██   ██    ██    ██      ███████
+ 
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define tt int t; cin >> t; while (t--)
+#define f(i, a) for (int i = 0; i < a; i++)
+#define fr(i, bg, a) for (int i = bg; i <=a; i++)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define ll long long
+#define endd cout << endl
+#define vec(v, n) vector<int> v(n)
+#define vc(v, n) vector<ll> v(n)
+#define srt(v) sort(v.begin(), v.end())
+#define rsrt(v) sort(v.rbegin(), v.rend())
+#define maxv *max_element(v.begin(),v.end())
+#define minv *min_element(v.begin(),v.end())
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+
+            int main(){
+            optimize();
+   tt{
+int n,x;
+cin>>n>>x;
+vector<int>v;
+if(x==n){
+      f(i,n) v.push_back(i);
+}
+else {
+      f(i,x)v.push_back(i);
+      for(int i=x+1;i<n;i++)v.push_back(i);
+      v.push_back(x);
+}
+for(auto u:v){
+      cout<<u<<" ";
+}
+cout<<endl;
+}}
+
+            
