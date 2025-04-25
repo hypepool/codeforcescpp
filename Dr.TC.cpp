4497a8b4e8e88e10abe515b56aacee0a955ddda1
hypepool <saminurpurob@gmail.com>
2025-04-25 11:33:05 +0600

    Add files via upload

---
+++ Dr.TC.cpp
@@ -0,0 +1,42 @@
+/*
+ 
+      ██   ██ ██    ██ ██████  ███████
+      ██   ██ ██    ██ ██  ██  ██
+      ███████ ████████ ██████  █████
+      ██   ██    ██    ██      ██
+      ██   ██    ██    ██      ███████
+ 
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define tt int t; cin >> t; while (t--)
+#define f(i, a) for (int i = 0; i < a; i++)
+#define fr(i, bg, a) for (int i = bg; i <=a; i++)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define ll long long
+#define endd cout << endl
+#define vec(v, n) vector<int> v(n)
+#define vc(v, n) vector<ll> v(n)
+#define srt(v) sort(v.begin(), v.end())
+#define rsrt(v) sort(v.rbegin(), v.rend())
+#define maxv *max_element(v.begin(),v.end())
+#define minv *min_element(v.begin(),v.end())
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+
+            int main(){
+            optimize();
+   tt{
+int n,cnt=0;
+string s;
+cin>>n>>s;
+for(auto c:s){
+      if(c=='1')cnt++;
+}
+cout<<(n+(n-2)*cnt)<<endl;
+
+      }
+}
+
+            
